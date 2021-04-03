3691f3bff179e333d5b055814b21042cd4f7cbca
Alnya <69311410+Alnya@users.noreply.github.com>
2021-04-04 06:59:09 +0900

    fix host
    fix host

--- README.md
+++ README.md
@@ -13,6 +13,17 @@ curl -X POST -d "num=
 00000000
 00000000"  http://127.0.0.1:5000/post
 ```
+```
+curl -X POST -d "num=
+00000000
+00000000
+00000000
+000-11000
+0001-1000
+00000000
+00000000
+00000000"  https://othello-arena-api.herokuapp.com/get
+```
 
 ## Get your moves
 ```
@@ -25,4 +36,15 @@ curl -X POST -d "num=
 00000000
 00000000
 00000000"  http://127.0.0.1:5000/get_moves
+```
+```
+curl -X POST -d "num=
+00000000
+00000000
+00000000
+000-11000
+0001-1000
+00000000
+00000000
+00000000"  https://othello-arena-api.herokuapp.com/get_moves
 ```
--- app.py
+++ app.py
@@ -8,6 +8,30 @@ from copy import deepcopy
 app = Flask(__name__)
 
 
+def posted_board(num):
+    error_message = "ValidationError"
+    ls = []
+    str_num = str(num)
+    frag = False
+    for i in range(len(str_num)):
+        if frag:
+            ls.append(-1)
+            frag = False
+            continue
+        if str_num[i] == "-":
+            frag = True
+        else:
+            ls.append(int(str_num[i]))
+    if len(ls) != 64:
+        return error_message
+    board = []
+    for i in range(8):
+        board.append([])
+        for j in range(8):
+            board[i].append(ls[i * 8 + j])
+    return board
+
+
 @app.route('/')
 def index():
     return 'OthelloArenaAPI'
@@ -20,49 +44,13 @@ def get():
 
 @app.route('/post', methods=['POST'])
 def post():
-    error_message = "ValidationError"
-    # name = request.form['name']
-    num = request.form['num']
-    ls = []
-    str_num = str(num)
-    frag = False
-    for i in range(len(str_num)):
-        if frag:
-            ls.append(-1)
-            frag = False
-            continue
-        if str_num[i] == "-":
-            frag = True
-        else:
-            ls.append(int(str_num[i]))
-
-    # ls = list(str(num))
-    # ls = [int(i) for i in ls]
-    # ans = []
-    # for i in range(2):
-    #     ans.append([])
-    #     for j in range(2):
-    #         ans[i].append(ls[2 * i + j])
-
-    if len(ls) != 64:
-        return error_message
-
-    board = []
-    for i in range(8):
-        board.append([])
-        for j in range(8):
-            board[i].append(ls[i * 8 + j])
-    # board[3][3] = -1
-    # board[3][4] = 1
-    # board[4][3] = 1
-    # board[4][4] = -1
+    board = posted_board(request.form['num'])
+    if isinstance(board, str):
+        return board
     moves = getMoves(board=deepcopy(board), player=-1, size=8)
     action = getAction(board=deepcopy(board), moves=moves)
     board = execute(board=deepcopy(board), action=action, player=-1, size=8)
     data = {
-        # 'name': name,
-        # 'num': num,
-        # 'ls': ls,
         'action': action,
         'board': board
     }
@@ -71,27 +59,9 @@ def post():
 
 @app.route('/get_moves', methods=['POST'])
 def get_moves():
-    error_message = "ValidationError"
-    num = request.form['num']
-    ls = []
-    str_num = str(num)
-    frag = False
-    for i in range(len(str_num)):
-        if frag:
-            ls.append(-1)
-            frag = False
-            continue
-        if str_num[i] == "-":
-            frag = True
-        else:
-            ls.append(int(str_num[i]))
-    if len(ls) != 64:
-        return error_message
-    board = []
-    for i in range(8):
-        board.append([])
-        for j in range(8):
-            board[i].append(ls[i * 8 + j])
+    board = posted_board(request.form['num'])
+    if isinstance(board, str):
+        return board
     moves = getMoves(board=deepcopy(board), player=1, size=8)
     data = {
         'moves': moves
@@ -100,4 +70,5 @@ def get_moves():
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=False)
+    # app.run(debug=True)
